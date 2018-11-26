b77b690133900717f7123d84b581e1661b1a64e0
Phoenix <rob.gallyamov@gmail.com>
2018-11-26 21:11:24 +0500

    template project

--- src/main/java/ru/phoenix/returned/counter/WordCounter.java
+++ src/main/java/ru/phoenix/returned/counter/WordCounter.java
@@ -9,41 +9,41 @@ import java.lang.Integer;
 public class WordCounter {
 
 	public static void main(String[] args) {
-		
-		//creating sample Collection
+
+		// creating sample Collection
 		List<Integer> myList = new ArrayList<Integer>();
-		for(int i=0; i<10; i++) myList.add(i);
-		
-		//traversing using Iterator
+		for (int i = 0; i < 10; i++)
+			myList.add(i);
+
+		// traversing using Iterator
 		Iterator<Integer> it = myList.iterator();
-		while(it.hasNext()){
+		while (it.hasNext()) {
 			Integer i = it.next();
-			System.out.println("Iterator Value::"+i);
+			System.out.println("Iterator Value::" + i);
 		}
-		
-		//traversing through forEach method of Iterable with anonymous class
+
+		// traversing through forEach method of Iterable with anonymous class
 		myList.forEach(new Consumer<Integer>() {
 
 			public void accept(Integer t) {
-				System.out.println("forEach anonymous class Value::"+t);
+				System.out.println("forEach anonymous class Value::" + t);
 			}
 
 		});
-		
-		//traversing with Consumer interface implementation
+
+		// traversing with Consumer interface implementation
 		MyConsumer action = new MyConsumer();
 		myList.forEach(action);
-		
+
 	}
 
 }
 
 //Consumer implementation that can be reused
-class MyConsumer implements Consumer<Integer>{
+class MyConsumer implements Consumer<Integer> {
 
 	public void accept(Integer t) {
-		System.out.println("Consumer impl Value::"+t);
+		System.out.println("Consumer impl Value::" + t);
 	}
-
 
 }
